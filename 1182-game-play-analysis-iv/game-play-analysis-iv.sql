c80ef95afb0569547a5d8f35104e875cdb91e1d9
ShubhamBarkale <shubhambarkale674@gmail.com>
2026-09-22 06:56:03 +0530

    Time: 3807 ms (7.69%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -0,0 +1,15 @@
+SELECT
+    ROUND(
+        COUNT(DISTINCT a.player_id) /
+        COUNT(DISTINCT b.player_id),
+        2
+    ) AS fraction
+FROM Activity b
+LEFT JOIN Activity a
+    ON a.player_id = b.player_id
+    AND a.event_date = DATE_ADD(b.event_date, INTERVAL 1 DAY)
+WHERE b.event_date = (
+    SELECT MIN(event_date)
+    FROM Activity
+    WHERE player_id = b.player_id
+);
